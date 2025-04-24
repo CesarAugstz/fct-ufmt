97380f1eacdb48ca8ed568f75e64f095c06db00d
cesaraugstz <dev.caugustoaf@gmail.com>
2025-04-23 21:34:07 -0400

    fix: build

--- src/app/home/news/[id]/page.tsx
+++ src/app/home/news/[id]/page.tsx
@@ -1,6 +1,11 @@
 import NewsDetail from '@/components/home/news/news-detail'
 
-export default async function NewsPage({ params }: { params: { id: string } }) {
+export default async function NewsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
   // TODO: fetch news item from database
-  return <NewsDetail id={params.id} />
+  const id = (await params).id
+  return <NewsDetail id={id} />
 }
